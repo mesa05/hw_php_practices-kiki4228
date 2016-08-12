acb066f8991c7e30b1e0f3964c3ebcb9d844c26e
Hinasaki <john410500198@gmail.com>
2016-08-12 01:50:56 +0000

    A&E done

---
+++ A_stars.php
@@ -0,0 +1,10 @@
+<?php
+        $stars = "";
+        for($layer = 0;$layer < 5; $layer ++){
+            
+        $stars .= "*";
+        
+        echo $stars."<br>";
+        }
+
+?>
---
+++ E_substr_count.php
@@ -0,0 +1,68 @@
+<?php
+$str="11525426983ABBCDFFFFFO";
+echo "原字串:".$str."<br />";
+$str1=substr_count($str,'1');
+echo "統計數字1出現次數:".$str1."<br />";
+$str2=substr_count($str,'2');
+echo "統計數字2出現次數:".$str2."<br />";
+$str3=substr_count($str,'3');
+echo "統計數字3出現次數:".$str3."<br />";
+$str4=substr_count($str,'4');
+echo "統計數字4出現次數:".$str4."<br />";
+$str5=substr_count($str,'5');
+echo "統計數字5出現次數:".$str5."<br />";
+$str6=substr_count($str,'6');
+echo "統計數字6出現次數:".$str6."<br />";
+$str7=substr_count($str,'7');
+echo "統計數字7出現次數:".$str7."<br />";
+$str8=substr_count($str,'8');
+echo "統計數字8出現次數:".$str8."<br />";
+$str9=substr_count($str,'9');
+echo "統計數字9出現次數:".$str9."<br />";
+$str0=substr_count($str,'0');
+echo "統計數字0出現次數:".$str0."<br />";
+$str11=substr_count($str,'A');
+echo "統計英文字A出現次數:".$str11."<br />";
+$str12=substr_count($str,'B');
+echo "統計英文字B出現次數:".$str12."<br />";
+$str13=substr_count($str,'C');
+echo "統計英文字C出現次數:".$str13."<br />";
+$str14=substr_count($str,'D');
+echo "統計英文字D出現次數:".$str14."<br />";
+$str15=substr_count($str,'E');
+echo "統計英文字E出現次數:".$str15."<br />";
+$str16=substr_count($str,'F');
+echo "統計英文字F出現次數:".$str16."<br />";
+$str17=substr_count($str,'G');
+echo "統計英文字G出現次數:".$str17."<br />";
+$str18=substr_count($str,'H');
+echo "統計英文字H出現次數:".$str18."<br />";
+$str19=substr_count($str,'I');
+echo "統計英文字I出現次數:".$str19."<br />";
+$str20=substr_count($str,'J');
+echo "統計英文字J出現次數:".$str20."<br />";
+$str21=substr_count($str,'K');
+echo "統計英文字K出現次數:".$str21."<br />";
+$str22=substr_count($str,'L');
+echo "統計英文字L出現次數:".$str22."<br />";
+$str23=substr_count($str,'M');
+echo "統計英文字M出現次數:".$str23."<br />";
+$str24=substr_count($str,'N');
+echo "統計英文字N出現次數:".$str24."<br />";
+$str25=substr_count($str,'O');
+echo "統計英文字O出現次數:".$str25."<br />";
+$str26=substr_count($str,'P');
+echo "統計英文字P出現次數:".$str26."<br />";
+
+
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        
+        <div id="output"></div>
+    </body>
+</html>
